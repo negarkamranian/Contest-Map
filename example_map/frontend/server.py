b6e422a02cb5b694ec790414980ab10ac7be79ff
negar.kamranian <negar.kamranian@divar.ir>
2025-07-06 19:21:01 +0330

    Modify map

--- example_map/frontend/server.py
+++ example_map/frontend/server.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
 # Configure Jinja2Templates
 templates = Jinja2Templates(directory="templates")
+
+# Mount static files (e.g., images) from the templates directory
+# This allows files like /static/image.png to be served directly.
+app.mount("/static", StaticFiles(directory="templates"), name="static")
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
